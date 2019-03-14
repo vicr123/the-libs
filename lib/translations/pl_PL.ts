97e2483059322f43b113cae319fdaf597c90d3af
feandesign <feandesign@protonmail.com>
2019-03-14 11:18:19 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: the-libs/the-libs
    Translate-URL: http://translate.vicr123.com/projects/the-libs/the-libs/pl/

--- lib/translations/pl_PL.ts
+++ lib/translations/pl_PL.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../tapplication.cpp" line="15"/>
         <source>Services</source>
-        <translation type="unfinished"></translation>
+        <translation>Usługi</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="16"/>
         <source>Hide %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukryj %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="17"/>
         <source>Hide Others</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukryj inne</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="18"/>
         <source>Show All</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokaż wszystko</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="19"/>
         <source>Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>Preferencje...</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="20"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>O %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="21"/>
         <source>Quit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyjdź z %1</translation>
     </message>
 </context>
 </TS>
