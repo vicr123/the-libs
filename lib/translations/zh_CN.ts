a06d993bbde735f6f0f3bcc22551d2397e5b9934
Oscar <blue@bluexguardian.com>
2020-05-16 13:43:35 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: the-libs/the-libs core
    Translate-URL: https://translate.vicr123.com/projects/the-libs/the-libs-core/zh_Hans/

--- lib/translations/zh_CN.ts
+++ lib/translations/zh_CN.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>MAC_APPLICATION_MENU</name>
     <message>
         <location filename="../tapplication.cpp" line="68"/>
         <source>Services</source>
-        <translation type="unfinished"></translation>
+        <translation>服务</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="69"/>
         <source>Hide %1</source>
-        <translation type="unfinished"></translation>
+        <translation>收起</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="70"/>
         <source>Hide Others</source>
-        <translation type="unfinished"></translation>
+        <translation>收起其他</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="71"/>
         <source>Show All</source>
-        <translation type="unfinished"></translation>
+        <translation>查看全部</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="72"/>
         <source>Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>偏好</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="73"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="74"/>
         <source>Quit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>退出</translation>
     </message>
 </context>
 <context>
@@ -44,52 +44,52 @@
     <message>
         <location filename="../tapplication.cpp" line="77"/>
         <source>Displays version information.</source>
-        <translation type="unfinished"></translation>
+        <translation>显示版本信息。</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="78"/>
         <source>Displays this help.</source>
-        <translation type="unfinished"></translation>
+        <translation>显示此帮助。</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="79"/>
         <source>Unknown option &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>未知的选项  ‘%1’.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="80"/>
         <source>Unknown options: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>未知的选项</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="81"/>
         <source>Missing value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>在。。。之后缺少数据</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="82"/>
         <source>Unexpected value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>在。。。之后发现了意外的数据</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="83"/>
         <source>[options]</source>
-        <translation type="unfinished"></translation>
+        <translation>选项</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="84"/>
         <source>Usage: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>用量</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="85"/>
         <source>Options:</source>
-        <translation type="unfinished"></translation>
+        <translation>选项：</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="86"/>
         <source>Arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>Arguments</translation>
     </message>
 </context>
 <context>
@@ -97,23 +97,23 @@
     <message>
         <location filename="../taboutdialog.ui" line="194"/>
         <source>SOFTWARE</source>
-        <translation type="unfinished"></translation>
+        <translation>软件</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="239"/>
         <source>COPYRIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation>版权</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="299"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>好的</translation>
     </message>
     <message>
         <location filename="../taboutdialog.cpp" line="14"/>
         <location filename="../taboutdialog.cpp" line="15"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="../tapplication.cpp" line="500"/>
         <source>Copyright © %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>版权</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="473"/>
@@ -133,47 +133,47 @@
         <location filename="../tapplication.cpp" line="491"/>
         <location filename="../tapplication.cpp" line="494"/>
         <source>Licensed under the terms of the %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>授权</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="473"/>
         <source>GNU General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权， 第三版</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="476"/>
         <source>GNU General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权， 第三版或之后</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="479"/>
         <source>GNU General Public License, version 2</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权，第二版</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="482"/>
         <source>GNU General Public License, version 2, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权，第二版或之后</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="485"/>
         <source>GNU Lesser General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权，第三版</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="488"/>
         <source>GNU Lesser General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权， 第三版或之后</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="491"/>
         <source>GNU Lesser General Public License, version 2.1</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权，第2.1版</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="494"/>
         <source>GNU Lesser General Public License, version 2.1, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU版权，2.1版或之后</translation>
     </message>
 </context>
 </TS>
