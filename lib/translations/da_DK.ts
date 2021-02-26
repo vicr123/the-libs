c9ee984115528b4f4b4b3bc4466869d87eb6f51a
Tobias SN <tobi897a2@outlook.dk>
2021-02-26 13:03:16 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (57 of 57 strings)
    
    Translation: the-libs/the-libs core
    Translate-URL: https://translate.vicr123.com/projects/the-libs/the-libs-core/da/

--- lib/translations/da_DK.ts
+++ lib/translations/da_DK.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="da_DK">
 <context>
     <name>DebugLogModel</name>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="267"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>Tidsstempler</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="269"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontekst</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="271"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Besked</translation>
     </message>
 </context>
 <context>
@@ -24,38 +24,38 @@
     <message>
         <location filename="../private/debuglogpopover.ui" line="35"/>
         <source>Debug Log Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejlfindings-log-element</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="35"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>Tidsstempel</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="36"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontekst</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="37"/>
         <source>Severity</source>
-        <translation type="unfinished"></translation>
+        <translation>Alvorlighed</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="38"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Fil</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="38"/>
         <location filename="../private/debuglogpopover.cpp" line="39"/>
         <source>[unavailable]</source>
-        <translation type="unfinished"></translation>
+        <translation>[utilgængelig]</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="39"/>
         <source>Function</source>
-        <translation type="unfinished"></translation>
+        <translation>Funktion</translation>
     </message>
 </context>
 <context>
@@ -63,28 +63,28 @@
     <message>
         <location filename="../private/debuglogwindow.ui" line="14"/>
         <source>Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejlfindings-log</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="80"/>
         <source>Search Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Søg i fejlfindings-log</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="156"/>
         <source>Clear Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Ryd log</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="183"/>
         <source>Filter by Context</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrer efter kontekst</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="301"/>
         <source>context</source>
         <comment>Placeholder in search box</comment>
-        <translation type="unfinished"></translation>
+        <translation>kontekst</translation>
     </message>
 </context>
 <context>
@@ -92,7 +92,7 @@
     <message>
         <location filename="../jobs/jobspopover.ui" line="35"/>
         <source>Jobs</source>
-        <translation type="unfinished"></translation>
+        <translation>Opgaver</translation>
     </message>
 </context>
 <context>
@@ -100,38 +100,38 @@
     <message>
         <location filename="../tapplication.cpp" line="93"/>
         <source>Services</source>
-        <translation type="unfinished"></translation>
+        <translation>Tjenester</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="94"/>
         <source>Hide %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Skjul %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="95"/>
         <source>Hide Others</source>
-        <translation type="unfinished"></translation>
+        <translation>Skjul andre</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="96"/>
         <source>Show All</source>
-        <translation type="unfinished"></translation>
+        <translation>Skjul alle</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="97"/>
         <source>Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>Indstillinger...</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="98"/>
         <location filename="../thelpmenu.cpp" line="46"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Om %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="99"/>
         <source>Quit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Luk %1</translation>
     </message>
 </context>
 <context>
@@ -139,52 +139,52 @@
     <message>
         <location filename="../tapplication.cpp" line="102"/>
         <source>Displays version information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Viser information om denne version.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="103"/>
         <source>Displays this help.</source>
-        <translation type="unfinished"></translation>
+        <translation>Viser denne hjælp.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="104"/>
         <source>Unknown option &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukendt valgmulighed &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="105"/>
         <source>Unknown options: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukendte valgmuligheder: %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="106"/>
         <source>Missing value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Manglende værdi efter &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="107"/>
         <source>Unexpected value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uforventet værdi efter &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="108"/>
         <source>[options]</source>
-        <translation type="unfinished"></translation>
+        <translation>[muligheder]</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="109"/>
         <source>Usage: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Brug: %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="110"/>
         <source>Options:</source>
-        <translation type="unfinished"></translation>
+        <translation>Muligheder:</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="111"/>
         <source>Arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>Parametre:</translation>
     </message>
 </context>
 <context>
@@ -192,23 +192,23 @@
     <message>
         <location filename="../taboutdialog.ui" line="194"/>
         <source>SOFTWARE</source>
-        <translation type="unfinished"></translation>
+        <translation>SOFTWARE</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="239"/>
         <source>COPYRIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation>OPHAVSRET</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="299"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OKAY</translation>
     </message>
     <message>
         <location filename="../taboutdialog.cpp" line="14"/>
         <location filename="../taboutdialog.cpp" line="15"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Om %1</translation>
     </message>
 </context>
 <context>
@@ -216,7 +216,7 @@
     <message>
         <location filename="../tapplication.cpp" line="562"/>
         <source>Copyright © %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ophavsret © %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="534"/>
@@ -228,47 +228,47 @@
         <location filename="../tapplication.cpp" line="552"/>
         <location filename="../tapplication.cpp" line="555"/>
         <source>Licensed under the terms of the %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Licenseret under betingelserne i %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="534"/>
         <source>GNU General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU General Public License, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="537"/>
         <source>GNU General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU General Public License, version 3, eller nyere</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="540"/>
         <source>GNU General Public License, version 2</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU General Public License, version 2</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="543"/>
         <source>GNU General Public License, version 2, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU General Public License, version 2, eller nyere</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="546"/>
         <source>GNU Lesser General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU Lesser General Public License, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="549"/>
         <source>GNU Lesser General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU Lesser General Public License, version 3, eller nyere</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="552"/>
         <source>GNU Lesser General Public License, version 2.1</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU Lesser General Public License, version 2.1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="555"/>
         <source>GNU Lesser General Public License, version 2.1, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU Lesser General Public License, version 2.1, eller senere</translation>
     </message>
 </context>
 <context>
@@ -276,27 +276,27 @@
     <message>
         <location filename="../thelpmenu.cpp" line="40"/>
         <source>%1 Help</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Hjælp</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="43"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Rapporter fejl</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="44"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Kildekode</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="45"/>
         <source>View Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis fejlfindings-log</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="73"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Hjælp</translation>
     </message>
 </context>
 <context>
@@ -305,31 +305,31 @@
         <location filename="../tlogger.cpp" line="96"/>
         <source>Debug</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>Fejlfinding</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="98"/>
         <source>Warning</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>Advarsel</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="100"/>
         <source>Critical</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>Kritisk</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="102"/>
         <source>Fatal</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>Fatal</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="104"/>
         <source>Info</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
 </context>
 </TS>
