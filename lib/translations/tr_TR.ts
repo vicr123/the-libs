4db71bbe3afcb5c8b1e47ca14e8a0a3f89ba1f69
Aleyna Tilki <atahanreis@icloud.com>
2021-03-09 01:34:24 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (59 of 59 strings)
    
    Translation: the-libs/the-libs core
    Translate-URL: https://translate.vicr123.com/projects/the-libs/the-libs-core/tr/

--- lib/translations/tr_TR.ts
+++ lib/translations/tr_TR.ts
@@ -304,12 +304,12 @@
     <message>
         <location filename="../tinputdialog.ui" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="../tinputdialog.ui" line="55"/>
         <source>OK</source>
-        <translation type="unfinished">PEKİ</translation>
+        <translation>TAMAM</translation>
     </message>
 </context>
 <context>
