63cd7bfafcd65524bef62d4a058931552f737075
owahai <owaisdabas61@gmail.com>
2021-07-08 02:42:19 +0000

    Translated using Weblate (Arabic (Saudi Arabia))
    Currently translated at 100.0% (59 of 59 strings)
    
    Translation: the-libs/the-libs core
    Translate-URL: https://translate.vicr123.com/projects/the-libs/the-libs-core/ar_SA/

--- lib/translations/ar_SA.ts
+++ lib/translations/ar_SA.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../private/debuglogwindow.cpp" line="267"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>الزمن</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="269"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>سياق الكلام</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="271"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>رسالة</translation>
     </message>
 </context>
 <context>
@@ -24,38 +24,38 @@
     <message>
         <location filename="../private/debuglogpopover.ui" line="35"/>
         <source>Debug Log Item</source>
-        <translation type="unfinished"></translation>
+        <translation>تسصحيح العنصر السجل</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="35"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>الزمن</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="36"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>سياق الكلام</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="37"/>
         <source>Severity</source>
-        <translation type="unfinished"></translation>
+        <translation>خطورة</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="38"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>ملف</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="38"/>
         <location filename="../private/debuglogpopover.cpp" line="39"/>
         <source>[unavailable]</source>
-        <translation type="unfinished"></translation>
+        <translation>[غير متاوفر]</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="39"/>
         <source>Function</source>
-        <translation type="unfinished"></translation>
+        <translation>دور</translation>
     </message>
 </context>
 <context>
@@ -63,28 +63,28 @@
     <message>
         <location filename="../private/debuglogwindow.ui" line="14"/>
         <source>Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>سجل التصحيح</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="80"/>
         <source>Search Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>دور سجل التصحيح</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="156"/>
         <source>Clear Log</source>
-        <translation type="unfinished"></translation>
+        <translation>أمسح الجل</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="183"/>
         <source>Filter by Context</source>
-        <translation type="unfinished"></translation>
+        <translation>صفية حسب السياق</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="301"/>
         <source>context</source>
         <comment>Placeholder in search box</comment>
-        <translation type="unfinished"></translation>
+        <translation>سياق الكلام</translation>
     </message>
 </context>
 <context>
@@ -92,7 +92,7 @@
     <message>
         <location filename="../jobs/jobspopover.ui" line="35"/>
         <source>Jobs</source>
-        <translation type="unfinished"></translation>
+        <translation>وظائف</translation>
     </message>
 </context>
 <context>
@@ -100,38 +100,38 @@
     <message>
         <location filename="../tapplication.cpp" line="93"/>
         <source>Services</source>
-        <translation type="unfinished"></translation>
+        <translation>خدمات</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="94"/>
         <source>Hide %1</source>
-        <translation type="unfinished"></translation>
+        <translation>إخفاء %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="95"/>
         <source>Hide Others</source>
-        <translation type="unfinished"></translation>
+        <translation>إخفاء الأخرين</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="96"/>
         <source>Show All</source>
-        <translation type="unfinished"></translation>
+        <translation>عرض الكل</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="97"/>
         <source>Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>التفضيلات ...</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="98"/>
         <location filename="../thelpmenu.cpp" line="46"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>حول %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="99"/>
         <source>Quit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>ترك %1</translation>
     </message>
 </context>
 <context>
@@ -139,52 +139,52 @@
     <message>
         <location filename="../tapplication.cpp" line="102"/>
         <source>Displays version information.</source>
-        <translation type="unfinished"></translation>
+        <translation>يعرض معلومات الإصدار.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="103"/>
         <source>Displays this help.</source>
-        <translation type="unfinished"></translation>
+        <translation>يعرض هذه المساعدة (help).</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="104"/>
         <source>Unknown option &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>خيار غير معلوم &apos;٪ 1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="105"/>
         <source>Unknown options: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>خيارت غير معلومة &apos;٪ 1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="106"/>
         <source>Missing value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>القيمة مفقودة بعد &apos;٪ 1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="107"/>
         <source>Unexpected value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>قيمة غير متوقعة بعد &apos;٪ 1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="108"/>
         <source>[options]</source>
-        <translation type="unfinished"></translation>
+        <translation>[أختيارات]</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="109"/>
         <source>Usage: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>الاستخدام:٪ 1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="110"/>
         <source>Options:</source>
-        <translation type="unfinished"></translation>
+        <translation>أختيارات:</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="111"/>
         <source>Arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>الحجج:</translation>
     </message>
 </context>
 <context>
@@ -192,23 +192,23 @@
     <message>
         <location filename="../taboutdialog.ui" line="194"/>
         <source>SOFTWARE</source>
-        <translation type="unfinished"></translation>
+        <translation>البرامج</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="239"/>
         <source>COPYRIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation>حقوق النشر</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="299"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>نعم</translation>
     </message>
     <message>
         <location filename="../taboutdialog.cpp" line="14"/>
         <location filename="../taboutdialog.cpp" line="15"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>حول %1</translation>
     </message>
 </context>
 <context>
@@ -216,7 +216,7 @@
     <message>
         <location filename="../tapplication.cpp" line="566"/>
         <source>Copyright © %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>حقوق النشر © %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="538"/>
@@ -228,47 +228,47 @@
         <location filename="../tapplication.cpp" line="556"/>
         <location filename="../tapplication.cpp" line="559"/>
         <source>Licensed under the terms of the %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>المرخصة بموجب شروط %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="538"/>
         <source>GNU General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص العام, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="541"/>
         <source>GNU General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص العام, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="544"/>
         <source>GNU General Public License, version 2</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص العام, version 2</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="547"/>
         <source>GNU General Public License, version 2, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص العام, version 2، أو لاحقا</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="550"/>
         <source>GNU Lesser General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص الأقل العام, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="553"/>
         <source>GNU Lesser General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص الأقل العام, version 3، أو لاحقا</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="556"/>
         <source>GNU Lesser General Public License, version 2.1</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص الأقل العام, version 2.1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="559"/>
         <source>GNU Lesser General Public License, version 2.1, or later</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU الترخيص الأقل العام, version 2.1، أو لاحقا</translation>
     </message>
 </context>
 <context>
@@ -276,27 +276,27 @@
     <message>
         <location filename="../thelpmenu.cpp" line="40"/>
         <source>%1 Help</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 مساعدة</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="43"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>إرسال مشكلة</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="44"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>مصادر</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="45"/>
         <source>View Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>عرض سجل التصحيح</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="73"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>مساعدة</translation>
     </message>
 </context>
 <context>
@@ -304,12 +304,12 @@
     <message>
         <location filename="../tinputdialog.ui" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>إلغاء</translation>
     </message>
     <message>
         <location filename="../tinputdialog.ui" line="55"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>نعم</translation>
     </message>
 </context>
 <context>
@@ -318,31 +318,31 @@
         <location filename="../tlogger.cpp" line="96"/>
         <source>Debug</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>التصحيح</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="98"/>
         <source>Warning</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>تحذير</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="100"/>
         <source>Critical</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>مهم</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="102"/>
         <source>Fatal</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>قاتلة</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="104"/>
         <source>Info</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>معلومات</translation>
     </message>
 </context>
 </TS>
