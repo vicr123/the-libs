efaa2c5f50addaae36bcb04a2a3814bae90e6bfd
CCSleep <captureme257@gmail.com>
2021-12-01 15:28:39 +0000

    Translated using Weblate (Thai)
    Currently translated at 71.1% (42 of 59 strings)
    
    Translation: the-libs/the-libs core
    Translate-URL: https://translate.vicr123.com/projects/the-libs/the-libs-core/th/

--- lib/translations/th_TH.ts
+++ lib/translations/th_TH.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../private/debuglogwindow.cpp" line="273"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>เวลา</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="275"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>หัวข้อ</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="277"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>ข้อความ</translation>
     </message>
 </context>
 <context>
@@ -24,38 +24,38 @@
     <message>
         <location filename="../private/debuglogpopover.ui" line="35"/>
         <source>Debug Log Item</source>
-        <translation type="unfinished"></translation>
+        <translation>ไอเท็มบันทึกการดีบัก</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="37"/>
         <source>Timestamp</source>
-        <translation type="unfinished"></translation>
+        <translation>เวลา</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="38"/>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>หัวข้อ</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="39"/>
         <source>Severity</source>
-        <translation type="unfinished"></translation>
+        <translation>ความรุนแรง</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="40"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>ไฟล์</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="40"/>
         <location filename="../private/debuglogpopover.cpp" line="41"/>
         <source>[unavailable]</source>
-        <translation type="unfinished"></translation>
+        <translation>[ไม่พบ]</translation>
     </message>
     <message>
         <location filename="../private/debuglogpopover.cpp" line="41"/>
         <source>Function</source>
-        <translation type="unfinished"></translation>
+        <translation>ฟังก์ชัน</translation>
     </message>
 </context>
 <context>
@@ -63,28 +63,28 @@
     <message>
         <location filename="../private/debuglogwindow.ui" line="14"/>
         <source>Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>บันทึกการดีบัก</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="80"/>
         <source>Search Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>ค้นหาบันทึกการดีบัก</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="156"/>
         <source>Clear Log</source>
-        <translation type="unfinished"></translation>
+        <translation>เคลียร์การบันทึก</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.ui" line="183"/>
         <source>Filter by Context</source>
-        <translation type="unfinished"></translation>
+        <translation>กรองโดยหัวขัอ</translation>
     </message>
     <message>
         <location filename="../private/debuglogwindow.cpp" line="307"/>
         <source>context</source>
         <comment>Placeholder in search box</comment>
-        <translation type="unfinished"></translation>
+        <translation>หัวข้อ</translation>
     </message>
 </context>
 <context>
@@ -92,7 +92,7 @@
     <message>
         <location filename="../jobs/jobspopover.ui" line="35"/>
         <source>Jobs</source>
-        <translation type="unfinished"></translation>
+        <translation>กำลังทำอยู่</translation>
     </message>
 </context>
 <context>
@@ -100,38 +100,38 @@
     <message>
         <location filename="../tapplication.cpp" line="95"/>
         <source>Services</source>
-        <translation type="unfinished"></translation>
+        <translation>เซอร์วิส</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="96"/>
         <source>Hide %1</source>
-        <translation type="unfinished"></translation>
+        <translation>ซ่อน %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="97"/>
         <source>Hide Others</source>
-        <translation type="unfinished"></translation>
+        <translation>ซ่อนหน้าต่างอื่น ๆ</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="98"/>
         <source>Show All</source>
-        <translation type="unfinished"></translation>
+        <translation>แสดงทั้งหมด</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="99"/>
         <source>Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>การตั้งค่าทั่วไป...</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="100"/>
         <location filename="../thelpmenu.cpp" line="46"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>เกี่ยวกับ %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="101"/>
         <source>Quit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>ปิด %1</translation>
     </message>
 </context>
 <context>
@@ -139,52 +139,52 @@
     <message>
         <location filename="../tapplication.cpp" line="104"/>
         <source>Displays version information.</source>
-        <translation type="unfinished"></translation>
+        <translation>แสดงข้อมูลเวอร์ชัน</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="105"/>
         <source>Displays this help.</source>
-        <translation type="unfinished"></translation>
+        <translation>แสดงข้อความการช่วยเหลือนี้</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="106"/>
         <source>Unknown option &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Unknown option &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="107"/>
         <source>Unknown options: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Unknown options: %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="108"/>
         <source>Missing value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Missing value after &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="109"/>
         <source>Unexpected value after &apos;%1&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Unexpected value after &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="110"/>
         <source>[options]</source>
-        <translation type="unfinished"></translation>
+        <translation>[ทางเลือก]</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="111"/>
         <source>Usage: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>วิธีใช้คำสั่ง: %1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="112"/>
         <source>Options:</source>
-        <translation type="unfinished"></translation>
+        <translation>ทางเลือก</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="113"/>
         <source>Arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>ตัวแปร</translation>
     </message>
 </context>
 <context>
@@ -192,23 +192,23 @@
     <message>
         <location filename="../taboutdialog.ui" line="194"/>
         <source>SOFTWARE</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">SOFTWARE</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="239"/>
         <source>COPYRIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">COPYRIGHT</translation>
     </message>
     <message>
         <location filename="../taboutdialog.ui" line="299"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">OK</translation>
     </message>
     <message>
         <location filename="../taboutdialog.cpp" line="15"/>
         <location filename="../taboutdialog.cpp" line="16"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>เกี่ยวกับ %1</translation>
     </message>
 </context>
 <context>
@@ -216,7 +216,7 @@
     <message>
         <location filename="../tapplication.cpp" line="583"/>
         <source>Copyright © %1.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Copyright © %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="555"/>
@@ -228,47 +228,47 @@
         <location filename="../tapplication.cpp" line="573"/>
         <location filename="../tapplication.cpp" line="576"/>
         <source>Licensed under the terms of the %1.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Licensed under the terms of the %1.</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="555"/>
         <source>GNU General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU General Public License, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="558"/>
         <source>GNU General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU General Public License, version 3, or later</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="561"/>
         <source>GNU General Public License, version 2</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU General Public License, version 2</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="564"/>
         <source>GNU General Public License, version 2, or later</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU General Public License, version 2, or later</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="567"/>
         <source>GNU Lesser General Public License, version 3</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU Lesser General Public License, version 3</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="570"/>
         <source>GNU Lesser General Public License, version 3, or later</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU Lesser General Public License, version 3, or later</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="573"/>
         <source>GNU Lesser General Public License, version 2.1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU Lesser General Public License, version 2.1</translation>
     </message>
     <message>
         <location filename="../tapplication.cpp" line="576"/>
         <source>GNU Lesser General Public License, version 2.1, or later</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GNU Lesser General Public License, version 2.1, or later</translation>
     </message>
 </context>
 <context>
@@ -276,27 +276,27 @@
     <message>
         <location filename="../thelpmenu.cpp" line="40"/>
         <source>%1 Help</source>
-        <translation type="unfinished"></translation>
+        <translation>ขอความช่วยเหลือ %1</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="43"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>บั๊กภายในไฟล์</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="44"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>ซอร์สโค้ด</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="45"/>
         <source>View Debug Log</source>
-        <translation type="unfinished"></translation>
+        <translation>ดูบันทึกการดีบัก</translation>
     </message>
     <message>
         <location filename="../thelpmenu.cpp" line="73"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>ช่วยเหลือ</translation>
     </message>
 </context>
 <context>
@@ -304,12 +304,12 @@
     <message>
         <location filename="../tinputdialog.ui" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>ยกเลิก</translation>
     </message>
     <message>
         <location filename="../tinputdialog.ui" line="55"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ตกลง</translation>
     </message>
 </context>
 <context>
@@ -318,31 +318,31 @@
         <location filename="../tlogger.cpp" line="105"/>
         <source>Debug</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>ดีบัก</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="107"/>
         <source>Warning</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>คำเตือน</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="109"/>
         <source>Critical</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>เกิดข้อผิดพลาด</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="111"/>
         <source>Fatal</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>เกิดความล้มเหลว</translation>
     </message>
     <message>
         <location filename="../tlogger.cpp" line="113"/>
         <source>Info</source>
         <comment>Severity Level</comment>
-        <translation type="unfinished"></translation>
+        <translation>แจ้งเตือน</translation>
     </message>
 </context>
 </TS>
